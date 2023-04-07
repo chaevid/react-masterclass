1e82d02d774b562b0d6856229299d521d8112480
chaevid <chaevid@gmail.com>
2023-04-07 21:44:38 +0900

    #2.3 'As'

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50%;
-`;
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal"></Box>
-      <Circle bgColor="tomato"></Circle>
+    <Father as="header">
+      <Btn>Log in</Btn>
+      <Btn
+        as="a"
+        href="/">
+        Log in
+      </Btn>
     </Father>
   );
 }
