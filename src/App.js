cd12facb396b6e5fc7ffa2882f1cb547d91d1f35
chaevid <chaevid@gmail.com>
2023-04-08 01:37:20 +0900

    #2.7 Themes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -6,51 +10,13 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-
-const animation = keyframes`
-  0%{
-    transform: rotate(0deg);
-    border-radius: 0%;
-  }
-  50%{
-    border-radius: 100%;
-  }
-  100%{
-    transform: rotate(360deg);
-    border-radius: 0%;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 4rem;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${animation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 5rem;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper as="header">
-      <Box>
-        <Emoji as="div">😎</Emoji>
-      </Box>
-      <Emoji as="div">🔥</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
