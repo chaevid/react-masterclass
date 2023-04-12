178e2a445ae701e80d0ec43af6ca9dce6fa3bd67
chaevid <chaevid@gmail.com>
2023-04-12 21:35:08 +0900

    Edit Theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ThemeProvider } from "styled-components";
-import App from "./App";
-import { theme } from "./theme";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+import { darkTheme } from './theme';
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={darkTheme}>
         <App />
       </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
---
+++ src/theme.tsx
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  bgColor: 'white',
+  textColor: 'black',
+  accentColor: '#FF7400',
+  cardBgColor: 'rbga(255, 255, 255, 0.1)',
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: 'black',
+  textColor: 'white',
+  accentColor: '#FFD118',
+  cardBgColor: 'rbga(0, 0, 0, 0.1)',
+};
